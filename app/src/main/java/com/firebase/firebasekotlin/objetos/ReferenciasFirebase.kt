66e128b2d51e4ce03c5c035d75bb18b59f9ba527
Jon Reyes <jonath.lt3@gmail.com>
2024-08-15 19:54:00 -0700

    Se termino el proyecto

---
+++ app/src/main/java/com/firebase/firebasekotlin/objetos/ReferenciasFirebase.kt
@@ -0,0 +1,7 @@
+package com.firebase.firebasekotlin.objetos
+
+object ReferenciasFirebase {
+    const val URL_DATABASE = "https://listafirebase-90276-default-rtdb.firebaseio.com/"
+    const val DATABASE_NAME = "agenda"
+    const val TABLE_NAME = "contactos"
+}
